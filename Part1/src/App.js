9b35111295cd867ab3a427628f17468959bb9959
Minwoo Gwak <mgwak2@illinois.edu>
2022-07-13 14:09:18 +0000

    Completed exercise 2.1

--- Part1/src/App.js
+++ Part1/src/App.js
@@ -1,89 +1,47 @@
-import { useState } from 'react'
+const Course =({course}) => {
+	return (
+		<div>
+			<h1>{course.name}</h1>
+			<div>
+				{course.parts.map(course => 
+					<Content key={course.id} course={course}/>	
+				)}
+			</div>
+		</div>		
+	)
 
+}
+
+const Content = ({course}) => {
+	return (
+		<div>{course.name} {course.exercises}</div>
+	)
+}
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-	
-  return (
-		<div>
-			<div>
-				<h1>Give Feedback</h1>
-			</div>
-			<div>
-				<button onClick={() => setGood(good + 1)}>Good</button>
-				<button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-				<button onClick={() => setBad(bad + 1)}>Bad</button>
-			</div>
-			<h1>Stats</h1>
-			<Statistics good={good} neutral={neutral} bad={bad}/>
-		</div>
-		
-  )
-}
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-const StatisticLine = (props) => {
-	return (
-		
-		<table>
-			<tbody>
-				<tr>
-					<td>{props.text}</td>
-					<td>{props.value}</td>
-				</tr>
-			</tbody>
-		</table>
-		
-	)
-	
+  return <Course course={course} />
 }
-
-const Statistics = (props) => {
-	const good = props.good
-	const neutral = props.neutral
-	const bad = props.bad
-	
-	return (
-		<div>
-			<StatisticLine text='good' value={good}/> 
-			<StatisticLine text='neutral' value={neutral}/> 
-			<StatisticLine text='bad' value={bad}/> 
-			<div>all: {good + neutral + bad}</div>
-			<CalAve good={good} neutral={neutral} bad={bad} />
-			<PosPer good={good} neutral={neutral} bad={bad} />
-		</div>
-	)
-}
-
-const CalAve = (props) => {
-	console.log('called CalAve!')
-	const good = props.good
-	const neutral = props.neutral
-	const bad = props.bad
-	
-	const ave = (good - bad) / (good+neutral+bad)
-	
-	if (good===0 && neutral===0 && bad===0) {
-		return <div>No feedBack given</div>
-	}
-	return <div>average: {ave}</div>
-}
-
-const PosPer = (props) => {
-	const good = props.good
-	const neutral = props.neutral
-	const bad = props.bad
-	
-	const percentage = good / (good+neutral+bad) * 100
-	
-	if (good===0 && neutral===0 && bad===0) {
-		return <div></div>
-	}
-	
-	return <div>positive percentage: {percentage}</div>
-}
-
 
 export default App
