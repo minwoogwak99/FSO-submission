52a09cdece59f33d8b547b68e4c7585498913453
Minwoo Gwak <mgwak2@illinois.edu>
2022-07-17 14:13:18 +0000

    completed till exercise 2.9

--- Part2/part2/src/App.js
+++ Part2/part2/src/App.js
@@ -2,11 +2,17 @@ import {useState} from 'react'
 
 function App() {
   const [person, setPerson] = useState([
-    {name: 'Minwoo Gwak', number: '01099426349'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+  const [filterList, setFilterList] = useState([])
+
+
 
   const addPerson = (e) => {
     e.preventDefault()
@@ -17,7 +23,7 @@ function App() {
     if ((newName.length === 0) || (newNumber.length === 0)) {
       alert('please fill in the blanks')
       console.log(nameArr)
-    } else if ((nameArr.includes(newName) !== true) || (numberArr.includes(newNumber) !== true)) {
+    } else if ((nameArr.includes(newName) !== true) && (numberArr.includes(newNumber) !== true)) {
       const newPerson = {
         name: newName,
         number: newNumber
@@ -31,9 +37,12 @@ function App() {
     }
   }
 
+
   const filterInfo = (e) => {
     e.preventDefault()
-    console.log(e.target)
+    setFilterList(person.filter(person => (person.name.toLowerCase().includes(filterValue.toLowerCase()))
+                                          || person.number.replace(/-/g, '').includes(filterValue) ))
+
   }
 
 
@@ -49,7 +58,13 @@ function App() {
   }
 
   const handleFilterValue = (e) => {
+    console.log(e.target.value)
     setFilterValue(e.target.value)
+    if (e.target.value.length === 0) {
+      setFilterList([])
+    }
+    
+    console.log(filterList)
   }
 
   
@@ -58,11 +73,14 @@ function App() {
   return (
     <div>
       <h2>PhoneBook</h2>
+      <h3>Search</h3>
       <div>
         <form onSubmit={filterInfo}>
           <div>
             filter: <input type='text' value={filterValue} onChange={handleFilterValue}/>
+            <button type='submit'>Search</button>
           </div>
+          
         </form>
       </div>
 
@@ -85,7 +103,9 @@ function App() {
 
       <h2>Numbers</h2>
       <div>
-        {person.map(person => <DisplayPersonName key={person.name} person={person}/>)}
+        {filterList.length > 0 ? filterList.map(person => <DisplayPersonName key={person.name} person={person}/>) 
+                                : person.map(person => <DisplayPersonName key={person.name} person={person}/>)
+        }
       </div>
      
     </div>
