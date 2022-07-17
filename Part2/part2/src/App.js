4b6d85234b38050f5048b8fb5c86b5f9abf09bc8
Minwoo Gwak <mgwak2@illinois.edu>
2022-07-17 11:09:37 +0000

    Completed until exercise 2.9

--- Part2/part2/src/App.js
+++ Part2/part2/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [person, setPerson] = useState([
+    {name: 'Minwoo Gwak', number: '01099426349'}
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setFilterValue] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    
+    const nameArr = person.map(person => person.name)
+    const numberArr = person.map(person => person.number)
+
+    if ((newName.length === 0) || (newNumber.length === 0)) {
+      alert('please fill in the blanks')
+      console.log(nameArr)
+    } else if ((nameArr.includes(newName) !== true) || (numberArr.includes(newNumber) !== true)) {
+      const newPerson = {
+        name: newName,
+        number: newNumber
+      }
+  
+      setPerson(person.concat(newPerson))
+      console.log(nameArr)
+    } else {
+      alert(`the information is already existing in the Phonebook!`)
+      console.log(nameArr, numberArr)
+    }
+  }
+
+  const filterInfo = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+
+
+  
+  const handleNameChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    console.log(e.target.value)
+    setNewNumber(e.target.value)
+  }
+
+  const handleFilterValue = (e) => {
+    setFilterValue(e.target.value)
+  }
+
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>PhoneBook</h2>
+      <div>
+        <form onSubmit={filterInfo}>
+          <div>
+            filter: <input type='text' value={filterValue} onChange={handleFilterValue}/>
+          </div>
+        </form>
+      </div>
+
+
+
+      <h2>Add new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input type='text' value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          Phone number: <input type='text' value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+
+
+
+      <h2>Numbers</h2>
+      <div>
+        {person.map(person => <DisplayPersonName key={person.name} person={person}/>)}
+      </div>
+     
     </div>
+
   );
+}
+
+
+const DisplayPersonName = ({person}) => {
+  return (
+    <div>{person.name} {person.number}</div>
+  )
 }
 
 export default App;
